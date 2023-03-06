c51ba427d8c8dd203e37edd469299e9072839e73
+ <+>
2023-03-06 21:05:17 +0300

    added inputs

--- src/App/MovieForm.js
+++ src/App/MovieForm.js
@@ -1,25 +1,27 @@
-import React, { useState } from "react";
+
+
+import React, { useState } from 'react';
 
 function MovieForm({ onAddMovie }) {
   const [formData, setFormData] = useState({
-    title: "",
-    description: "",
-    image: ""
-    });
+    title: '',
+    description: '',
+    image: ''
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
-    fetch("http://localhost:9292/movies", {
-      method: "POST",
+    fetch('http://localhost:9292/movies', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(formData),
+      body: JSON.stringify(formData)
     })
-      .then((res) => res.json())
-      .then((data) => {
+      .then(res => res.json())
+      .then(data => {
         onAddMovie(data);
-        setFormData({ title: "", description: "", image: ""});
+        setFormData({ title: '', description: '', image: '' });
       })
       .catch(console.error);
   }
@@ -28,35 +30,18 @@ function MovieForm({ onAddMovie }) {
     setFormData({ ...formData, [event.target.name]: event.target.value });
   }
 
+  
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="title"
-        placeholder="Title"
-        required
-        value={formData.title}
-        onChange={handleInputChange}
-      />
-      <input
-        type="text"
-        name="description"
-        placeholder="Description"
-        required
-        value={formData.description}
-        onChange={handleInputChange}
-      />
-      <input
-        type="text"
-        name="image"
-        placeholder="Image URL"
-        required
-        value={formData.image}
-        onChange={handleInputChange}
-      />
+      <input type="text" name="title" placeholder="Title" required value={formData.title} onChange={handleInputChange} />
+      <input type="text" name="description" placeholder="Description" required value={formData.description} onChange={handleInputChange} />
+      <input type="text" name="image" placeholder="Image URL" required value={formData.image_url} onChange={handleInputChange} />
+      <input type="text" name="genre" placeholder="Genre" required value={formData.genre} onChange={handleInputChange} />
+      <input type="ineger" name="rating" placeholder="Rating" required value={formData.rating} onChange={handleInputChange} />
+
       <button type="submit">Add Movie</button>
     </form>
   );
 }
 
-export default MovieForm;
+export default MovieForm
