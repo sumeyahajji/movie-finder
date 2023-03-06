b81be61990ad5933943d669440ef67050c74f32d
+ <+>
2023-03-06 21:07:39 +0300

    fetched url from backend

--- src/App/MovieList.js
+++ src/App/MovieList.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Movie from './Movie';
-import Navbar from './Navbar';
 import MovieForm from './MovieForm';
-import Search from './Search';
-
-
+import Search from './Search'
 
 function MovieList() {
   const [movies, setMovies] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
+  
 
   useEffect(() => {
     fetch('http://localhost:9292/movies')
@@ -16,20 +14,17 @@ function MovieList() {
       .then(data => setMovies(data))
       .catch(console.error);
   }, []);
-
   function handleAddMovie(movie) {
     setMovies([...movies, movie]);
   }
-
   function handleDeleteMovie(id) {
     setMovies(movies.filter(movie => movie.id !== id));
     setSearchResults(searchResults.filter(movie => movie.id !== id));
   }
   function handleSearch(query) {
-    setMovies(movies.filter((movie) => movie.title.toLowerCase() === query.toLowerCase()));
-      
-    }
- 
+    setMovies(movies.filter((movie) => movie.title.toLowerCase() === query));
+  }
+
  return (
     <div>
       <MovieForm onAddMovie={handleAddMovie} />
@@ -48,5 +43,4 @@ function MovieList() {
     </div>
   );
 }
-
 export default MovieList;
