99f8506945032698a4fd94b0f87d953c7e29634b
+ <+>
2023-03-07 15:20:05 +0300

    done

--- src/App/Search.js
+++ src/App/Search.js
@@ -1,19 +1,21 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 function Search({ onSearch }) {
-const [searchQuery, setSearchQuery] = useState('');
-const handleSearch = () => {
-const query = searchQuery.trim();
-onSearch(query);
-};
-return (
-<div>
-<input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder="Search movies..." />
-<button onClick={handleSearch}>Search</button>
-</div>
-);
+  const [searchQuery, setSearchQuery] = useState("");
+  const handleSearch = () => {
+    const query = searchQuery.trim();
+    onSearch(query);
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="Search movies..."
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
 }
 export default Search;
-
-
